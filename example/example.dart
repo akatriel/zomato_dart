30dc10064dc47cade608e1f4bd83315cb0f7b34d
akatriel <michael.c.gabay@gmail.com>
2020-03-17 00:47:34 -0400

    Added method for GETting cities. Added models directory and moved convenience class into there. Added fromJson methods to models. Extracted get request into its own method => sendRequest.

--- example/example.dart
+++ example/example.dart
@@ -5,6 +5,9 @@ void main(List<String> args) async {
   String userKey = args.first;
   print('Your Zomato user-key: $userKey');
 
-  var cats = await ZomatoDart(userKey).categories();
-  print(cats.first.name);
+  // var cats = await ZomatoDart(userKey).categories();
+  // print(cats.first.name);
+
+  var cities = await ZomatoDart(userKey).cities();
+  print(cities.length);
 }
--- lib/zomato_dart.dart
+++ lib/zomato_dart.dart
@@ -1,5 +1,6 @@
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
+import 'package:zomato_dart/models/models.dart';
 
 /// An API wrapper in Dart for the Zomato API
 class ZomatoDart {
@@ -13,16 +14,19 @@ class ZomatoDart {
   /// Map for headers and other params to be added to
   Map<String, String> _headersMap;
 
-  ZomatoDart(this._userKey) {
+  ZomatoDart(this._userKey, {json = true}) {
     if (_userKey == null || _userKey == '') {
       throw (Exception('user-key not provided'));
     }
-    _headersMap = new Map<String, String>.from(
-        {'user-key': _userKey, 'Accept': 'application/json'});
+
+    _headersMap = {
+      'user-key': _userKey,
+      'Accept': 'application/' + (json ? 'json' : 'xml')
+    };
   }
 
   Future<List<Category>> categories() async {
-    String endpoint =  '/categories';
+    String endpoint = '/categories';
     String uri = _baseUri + endpoint;
 
     http.Response response = await sendRequest(uri, endpoint);
@@ -48,7 +52,7 @@ class ZomatoDart {
         for (var cat in json['categories']) {
           var innerCat = cat['categories'];
           if (innerCat != null) {
-            categories.add(Category(innerCat['id'], innerCat['name']));
+            categories.add(Category.fromJson(innerCat));
           }
         }
 
@@ -62,35 +66,74 @@ class ZomatoDart {
     return categories;
   }
 
-  Future<List<City>> cities(){
+
+  /// return a list of cities based on the provided paramaters
+  /// The cities returned are found under the "location_suggestions" key
+  Future<List<City>> cities(
+      {String cityName,
+      String latitude,
+      String longitude,
+      List<int> cityIds,
+      int count}) async {
     String endpoint = '/cities';
     String uri = _baseUri + endpoint;
+    List<City> cities;
+    
+    Map<String, String> paramsMap = {
+      'q': cityName,
+      'lon' : longitude,
+      'lat' : latitude,
+      'city_ids' : cityIds?.join(', '),
+      'count' : count?.toString()
+    };
+    
+    // remove params not provided
+    paramsMap.removeWhere((k,v) => v == null);
+    
+    try {
+      var response = await sendRequest(uri, endpoint, paramsMap: paramsMap);
+      print('Zomato Response Status Code: ${response?.statusCode}');
 
+      cities = List<City>();
+
+      if (response.statusCode == 200) {
+        var jsonDecoded = convert.jsonDecode(response.body);
+        // _InternalLinkedHashMap<String, dynamic>
+        // print(jsonDecoded.runtimeType);
+        if (jsonDecoded['location_suggestions'] != null ){
+          for(var c in jsonDecoded['location_suggestions']) {
+            cities.add(City.fromJson(c));
+          }
+        }
+      }
+    } catch (e) {
+      print("There was an error extracting cities from the response.");
+      print(e);
+    }
+
+    return cities;
   }
 
-  Future<http.Response> sendRequest(String uri, String endpoint) async {
+  Future<http.Response> sendRequest(String uri, String endpoint,
+      {Map<String, String> paramsMap, Map<String, String> headersMap}) async {
     print("Fetching response from Zomato API for endpoint: $endpoint");
-    http.Response response = await client.get(uri, headers: _headersMap);
+
+    List<String> params;
+    // Build params string if params are present
+    if (paramsMap != null) {
+      paramsMap.removeWhere((k,v) => v == null);
+      params = List<String>();
+
+      if(!paramsMap.isEmpty) {     
+        paramsMap.forEach((k, v) {
+          params.add('$k=$v');
+        });
+      }
+    }
+    print(uri + '?' + (params.isEmpty ? '' : params.join('&')));
+    http.Response response =
+        await client.get(uri + '?' + (params.isEmpty ? '' : params.join('&')), headers: _headersMap);
+
     return response;
   }
-}
-
-class Category {
-  int id;
-  String name;
-  Category(this.id, this.name);
-}
-
-
-class City {
-  int id;
-  String name;
-  int countryId;
-  String countryName;
-  bool isState;
-  int stateId;
-  String stateName;
-  String stateCode;
-  City(this.id, this.name, this.countryId, this.countryName, this.isState,
-      this.stateId, this.stateName, this.stateCode);
 }
