e3192f0f9cfe78370cfa0bfeaf942aec3a60f0b4
akatriel <michael.c.gabay@gmail.com>
2020-03-17 15:29:38 -0400

    Added method and model for collections wrapper. Added InvalidArgumentException.

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -31,3 +31,23 @@ class City {
     stateCode = json['state_code'];
   }
 }
+
+class Collection {
+  int id;
+  int resCount;
+  String imageUrl;
+  String url;
+  String title;
+  String description;
+  String shareUrl;
+
+  Collection.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    resCount = json['res_count'];
+    imageUrl = json['image_url'];
+    url = json['url'];
+    title = json['title'];
+    description = json['description'];
+    shareUrl = json['shareUrl'];
+  }
+}
--- lib/zomato_dart.dart
+++ lib/zomato_dart.dart
@@ -19,7 +19,7 @@ class ZomatoDart {
   // TODO: complete logging
   ZomatoDart(this._userKey, {json = true, logging = true}) {
     if (_userKey == null || _userKey == '') {
-      throw (Exception('user-key not provided'));
+      throw (InvalidArgumentsException('user-key not provided'));
     }
 
     _headersMap = {
@@ -27,23 +27,24 @@ class ZomatoDart {
       'Accept': 'application/' + (json ? 'json' : 'xml')
     };
   }
-
+  /// Get a list of categories
   Future<List<Category>> categories() async {
     String endpoint = '/categories';
     String uri = _baseUri + endpoint;
 
-    http.Response response = await sendRequest(uri, endpoint);
-
     List<Category> categories;
+    try {
+      http.Response response = await _sendRequest(uri, endpoint);
 
-    if (response.statusCode == 200) {
-      categories = _extractCategories(response.body);
-    } else {
-      print(response);
-      print("The Zomato response was unsuccessful");
+      if (response.statusCode == 200) {
+        categories = _extractCategories(response.body);
+      } else {
+        _printBadResponse(response);
+      }
+    } catch (e) {
+      _printExceptionMessage(e, endpoint);
     }
 
-    _client?.close();
     return categories;
   }
 
@@ -69,8 +70,8 @@ class ZomatoDart {
     return categories;
   }
 
-  /// return a list of cities based on the provided paramaters
-  /// The cities returned are found under the "location_suggestions" key
+  /// Find the Zomato ID and other details for a city.
+  /// Return a list of cities based on the provided paramaters
   Future<List<City>> cities(
       {String cityName,
       String latitude,
@@ -93,7 +94,7 @@ class ZomatoDart {
     paramsMap.removeWhere((k, v) => v == null);
 
     try {
-      var response = await sendRequest(uri, endpoint, paramsMap: paramsMap);
+      var response = await _sendRequest(uri, endpoint, paramsMap: paramsMap);
       print('Zomato Response Status Code: ${response?.statusCode}');
 
       cities = List<City>();
@@ -107,28 +108,84 @@ class ZomatoDart {
             cities.add(City.fromJson(c));
           }
         }
+      } else {
+        _printBadResponse(response);
       }
     } catch (e) {
-      print("There was an error extracting cities from the response.");
-      print(e);
+      _printExceptionMessage(e, endpoint);
     }
 
     return cities;
   }
 
-  Future<http.Response> sendRequest(String uri, String endpoint,
+  void _printExceptionMessage(e, String endpoint) {
+    print("There an error extracting objects from $endpoint");
+    print(e);
+  }
+
+  /// Returns Zomato Restaurant Collections in a City.
+  /// Requires either city_id or lat/lon.
+  Future<List<Collection>> collections({int cityId, String latitude, String longitude, int count}) async {
+    _validateCollectionsParameters(cityId, latitude, longitude);
+
+    Map<String, String> paramsMap = {
+      'city_id': cityId?.toString(),
+      'lat': latitude,
+      'lon': longitude,
+      'count': count?.toString()
+    };
+
+    String endpoint = '/collections';
+    String uri = _baseUri + endpoint;
+    http.Response response =
+        await _sendRequest(uri, endpoint, paramsMap: paramsMap);
+
+    List<Collection> collections;
+    if (response.statusCode == 200) {
+      collections = List<Collection>();
+      var decodedJson = convert.jsonDecode(response.body);
+
+      for (var c in decodedJson['collections']) {
+        collections.add(Collection.fromJson(c['collection']));
+      }
+    } else {
+      _printBadResponse(response);
+    }
+
+    return collections;
+  }
+
+  void _validateCollectionsParameters(int cityId, String latitude, String longitude) {
+    if (cityId == null) {
+      if (latitude == null && longitude == null)
+      {
+        throw InvalidArgumentsException("cityId or lat/lon is null and must be provided.");
+      } 
+      else if (latitude == null || longitude == null)
+      {
+        throw InvalidArgumentsException("latitude and longitude must be provided together, or provide a cityId.");
+      }
+    }
+  }
+
+  void _printBadResponse(http.Response response) {
+    print('Status Code: ${response.statusCode}');
+    print(response.body);
+    print("The Zomato response was unsuccessful");
+  }
+
+  Future<http.Response> _sendRequest(String uri, String endpoint,
       {Map<String, String> paramsMap, Map<String, String> headersMap}) async {
     print("Fetching response from Zomato API for endpoint: $endpoint");
 
     List<String> params = _buildParamsList(paramsMap);
-    
+
     // build url from params string
     String url = uri + '?' + (params.isEmpty ? '' : params.join('&'));
     print(url);
-    
-    http.Response response = await _client.get(
-        url,
-        headers: _headersMap);
+
+    http.Response response = await _client.get(url, headers: _headersMap);
+    _client?.close();
 
     return response;
   }
@@ -146,5 +203,15 @@ class ZomatoDart {
       }
     }
     return params;
+  }
+}
+
+class InvalidArgumentsException implements Exception {
+  String message;
+  InvalidArgumentsException(String this.message) {}
+
+  @override
+  String toString() {
+    return message;
   }
 }
